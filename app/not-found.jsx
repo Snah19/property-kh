1bdb87d9f1ea2d16f7bbe253f2471c57bd0dd937
Snah <vibolveasna2004@gmail.com>
2025-08-08 01:40:41 +0700

    Implemented error and not found pages

---
+++ app/not-found.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { FaExclamationCircle } from "react-icons/fa";
+
+const NotFoundPage = () => {
+  return (
+    <section className="container max-w-2xl mx-auto pt-24">
+      <div className="py-24 px-6 shadow-md rounded-md border bg-white">
+        <div className="flex justify-center items-center gap-x-3 mb-10">
+          <FaExclamationCircle className="fas fa-exclamation-triangle fa-5x text-3xl text-yellow-400" />
+          <h1 className="text-3xl font-bold">404 Not found!</h1>
+        </div>
+        
+        <div className="text-center">
+          <p className="mb-10 text-xl text-gray-500">The page you are looking for does not exist.</p>
+          <Link className="py-4 px-6 rounded bg-blue-500 hover:bg-blue-600 text-white" href="/">Back to Home</Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NotFoundPage;
--- app/update-property/[propertyId]/page.jsx
+++ app/update-property/[propertyId]/page.jsx
@@ -1,12 +1,17 @@
 import UpdatePropertyForm from "@/components/update-property-form";
 import connectToMongoDB from "@/config/mongodb";
 import Property from "@/models/property";
+import { getSessionUser } from "@/utils/get-session-user";
 
 const UpdatePropertyPage = async ({ params }) => {
   await connectToMongoDB();
 
   const { propertyId } = await params;
   const property = await Property.findById(propertyId);
+
+  const { userId } = await getSessionUser();
+
+  if (property.owner.toString() !== userId) throw new Error("You can't update this property!");
 
   return (
     <section className="container max-w-2xl mx-auto py-24">
