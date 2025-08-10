0d3a538b89d5c7e7f3d0bb7f37bf2564ce774000
Snah <vibolveasna2004@gmail.com>
2025-08-10 07:29:33 +0700

    reverted authOptions

--- utils/auth-options.js
+++ utils/auth-options.js
@@ -3,9 +3,6 @@ import GoogleProvider from "next-auth/providers/google";
 import User from "@/models/user";
 
 export const authOptions = {
-  session: {
-    strategy: "jwt", // ✅ Required for middleware to work properly
-  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -14,32 +11,30 @@ export const authOptions = {
         params: {
           prompt: "consent",
           access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
+          response_type: "code"
+        } 
+      }
+    })
   ],
   callbacks: {
     async signIn({ profile }) {
-      const { email, name, picture: image } = profile;
+      const { email, name, picture: image} = profile;
       const username = name.slice(0, 20);
 
       await connectToMongoDB();
       const userExists = await User.findOne({ email });
 
       if (!userExists) {
-        await User.create({ email, username, image });
+        await User.create({ email, username, image });   
       }
 
       return true;
     },
-
-    async session({ session, token }) {
-      // Attach user ID from token
-      await connectToMongoDB();
-      const user = await User.findOne({ email: session.user.email });
+    
+    async session({ session }) {
+      const user = await User.findOne({email: session.user.email});
       session.user.id = user._id.toString();
       return session;
-    },
+    }
   },
 };
